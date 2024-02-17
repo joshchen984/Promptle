ff6c9aabee4cb1a738c88001d0c726a2827f65f0
joshchen984 <joshchen984@gmail.com>
2024-02-16 22:05:57 -0500

    Connect to mongo and create generate game route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 import os
-import os
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import urllib
+
+load_dotenv()
 
 
 def create_app():
     """Creates configured app"""
-
     app = Flask(__name__)
+    mongo_uri = f"mongodb+srv://{urllib.parse.quote(os.environ.get('MONGO_USERNAME'))}:{urllib.parse.quote(os.environ.get('MONGO_PASSWORD'))}@cluster0.qwottkf.mongodb.net/?retryWrites=true&w=majority"
+    app.config["MONGO_URI"] = mongo_uri
+    mongo = PyMongo(app)
     return app
 
 
--- backend/app/views.py
+++ backend/app/views.py
@@ -6,8 +6,7 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()
-client = openai.OpenAI()
+from app.generate import generate_image, generate_keywords, generate_prompt
 
 
 @app.route("/", methods=["GET"])
@@ -15,41 +14,8 @@ def index():
     return render_template("game.html")
 
 
-@app.route("/generate/prompt", methods=["GET"])
-def generate_prompt():
-    keywordsArg = request.args.get("keywords")
-
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a prompt engineer and an expert in creating prompts for Dall-e.",
-            },
-            {
-                "role": "user",
-                "content": f"Generate a dall e prompt that incorporates the following keywords: [{keywordsArg}]",
-            },
-        ],
-        max_tokens=30,
-    )
-    return completion.choices[0].message
-
-
-@app.route("/generate/keywords", methods=["GET"])
-def generate_keywords():
-    return ""
-
-
-@app.route("/generate/images", methods=["GET"])
-def generate_image():
-    prompt = request.args.get("prompt")
-    response = client.images.generate(
-        model="dall-e-2",
-        prompt=prompt,
-        size="512x512",
-        quality="standard",
-        n=1,
-    )
-    image_url = response.data[0].url
-    return image_url
+@app.route("/generate/game", methods=["POST"])
+def generate_game():
+    keywords = generate_keywords()
+    prompt = generate_prompt(keywords)
+    image = generate_image(prompt)
